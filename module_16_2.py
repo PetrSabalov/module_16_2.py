b4767df34006c73c0fad230aea0a3e02ae369342
PetrSabalov <petrsabalov@gmail.com>
2024-10-26 14:53:06 +0300

    Create module_16_2.py

---
+++ module_16_2.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get ('/')
+async def welcome():
+    return {'message':'Главная страница'}
+
+@app.get ('/user/admin')
+async def admin():
+    return {'message':'Вы вошли как администратор'}
+
+@app.get("/user/{user_id}")
+async def news(user_id:Annotated[int,Path(ge=1,le=100, description="Enter User ID",example='1')]):
+    return {'message':f'Вы вошли как пользователь № ,{user_id}'}
+
+@app.get("/user/{username}/{age}")
+async def news2(username:Annotated[str,Path(min_length=5,max_length=20, description="Enter username",example='UrbanUser')],
+                age:Annotated[int,Path(ge=18,le=120,description="Enter age", example='24')]):
+    return {'message':f'Вы вошли как пользователь  ,{username}, вам {age} лет'}
